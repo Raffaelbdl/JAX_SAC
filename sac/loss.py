d8496130e1b757c80aa79fc347cba3fd0dd1b6d4
Raffaelbdl <raffaelbdl@gmail.com>
2023-05-04 09:19:37 +0900

    ⚡ Use gaussian tanh

--- sac/loss.py
+++ sac/loss.py
@@ -1,10 +1,11 @@
 from functools import partial
 
 import jax.numpy as jnp
+from sac.distributions import normal_and_tanh_sample_and_log_prob
 
 
-def sac_loss_fn(config, actor_fwd, critic_fwd, params, batch):
-    def critic_loss_fn(params, batch):
+def get_sac_loss_fn(config, actor_fwd, critic_fwd):
+    def critic_loss_fn(params, key, batch):
         observations = batch["observations"]
         actions = batch["actions"]
         targets = batch["targets"]
@@ -15,28 +16,16 @@ def sac_loss_fn(config, actor_fwd, critic_fwd, params, batch):
 
         return q1_loss + q2_loss, {"q1_loss": q1_loss, "q2_loss": q2_loss}
 
-    def actor_loss_fn(params, batch):
+    def actor_loss_fn(params, key, batch):
         observations = batch["observations"]
-        actions = batch["actions"]
-        q1_values, q2_values = batch["q1_values"], batch["q2_values"]
+        dists = actor_fwd(params, None, observations)
+        actions, log_probs = normal_and_tanh_sample_and_log_prob(key, dists)
 
-        # TODO check if actions must be sampled again
-        log_probs = actor_fwd(params, None, observations).log_prob(actions)
-        actor_loss = jnp.mean(
-            jnp.fmin(q1_values, q2_values) - config["alpha"] * log_probs
-        )
+        q1, q2 = critic_fwd(batch["critic_params"], None, observations, actions)
+
+        log_probs = jnp.sum(log_probs, axis=-1, keepdims=True)
+        actor_loss = -jnp.mean(jnp.fmin(q1, q2) - config["alpha"] * log_probs)
 
         return actor_loss, {"actor_loss": actor_loss}
 
-    actor_loss, actor_loss_dict = actor_loss_fn(params, batch)
-    critic_loss, critic_loss_dict = critic_loss_fn(params, batch)
-
-    total_loss = actor_loss + critic_loss
-    loss_dict = actor_loss_dict
-    loss_dict.update(critic_loss_dict)
-
-    return total_loss, loss_dict
-
-
-def get_sac_loss_fn(config: dict, actor_fwd, critic_fwd):
-    return partial(sac_loss_fn, config, actor_fwd, critic_fwd)
+    return actor_loss_fn, critic_loss_fn
